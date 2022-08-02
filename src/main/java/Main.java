9f1b815157f1e077210708c2520f1e656719de44
Your ivan <isnytko@solvd.com>
2022-08-02 17:09:09 +0300

    add logger

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,15 @@
 package main.java;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(Main.class);
+        logger.trace("Trace");
+        logger.debug("Debug");
+        logger.info("Info");
+        logger.error("Error");
+    }
 }
